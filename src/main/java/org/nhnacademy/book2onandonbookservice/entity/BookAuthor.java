cce1070e67d41c533703b1aa6139974f02304332
Minseo Kim <porori031224@gmail.com>
2025-11-17 16:29:04 +0900

    Feat: BookAuthor, Author Entity 생성

---
+++ src/main/java/org/nhnacademy/book2onandonbookservice/entity/BookAuthor.java
@@ -0,0 +1,36 @@
+package org.nhnacademy.book2onandonbookservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Book-Author")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookAuthor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_author_id")
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+}
