905dc94f6ebfc78f2ac00169404dc6a6e0869360
Minseo Kim <porori031224@gmail.com>
2025-11-17 16:54:27 +0900

    Feat: Publisher Entity 구현

---
+++ src/main/java/org/nhnacademy/book2onandonbookservice/entity/Publisher.java
@@ -0,0 +1,35 @@
+package org.nhnacademy.book2onandonbookservice.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Publisher")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "publisher_id")
+    private Long id;
+
+    @Column(name = "publisher_name", length = 50, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "publisher", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<BookPublisher> bookPublishers = new ArrayList<>();
+}
