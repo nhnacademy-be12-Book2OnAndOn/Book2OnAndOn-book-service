d224d794e74f9ef075b90c3b59d22aae5cb119c7
onyune <wjsdbwls0303@gmail.com>
2025-11-17 13:00:27 +0900

    dev initial commit

--- src/main/java/org/nhnacademy/book2onandonbookservice/Book2OnAndOnBookServiceApplication.java
+++ src/main/java/org/nhnacademy/book2onandonbookservice/Book2OnAndOnBookServiceApplication.java
@@ -2,8 +2,10 @@ package org.nhnacademy.book2onandonbookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Book2OnAndOnBookServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/nhnacademy/book2onandonbookservice/controller/controller.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.book2onandonbookservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
