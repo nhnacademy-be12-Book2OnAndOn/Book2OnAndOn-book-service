e117aa1b6d4b4fa2c96411309fb06ac1bc635055
onyune <wjsdbwls0303@gmail.com>
2025-11-27 11:29:14 +0900

    Fix: OrderService endpoint modify

--- src/main/java/org/nhnacademy/book2onandonbookservice/client/OrderServiceClient.java
+++ src/main/java/org/nhnacademy/book2onandonbookservice/client/OrderServiceClient.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+//주문쪽에 요청 보낼때만 사용
 @FeignClient(name = "ORDER-SERVICE")
 public interface OrderServiceClient {
 
-    @GetMapping("/api/orders/check-purchase/{bookId}")
+    @GetMapping("orders/check-purchase/{bookId}")
     boolean hasPurchased(@RequestHeader("X-User-Id") Long userId, @PathVariable Long bookId);
 }
